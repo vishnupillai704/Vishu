deb7f135939d87dbeabe66407659733dce9f04d9
vishnupillai704 <vishnupillai704@gmail.com>
2022-02-10 16:55:58 +0530

    ASSIGNMENT COMPLETED

---
+++ Assignment/momgo2.js
@@ -0,0 +1,61 @@
+#ATLANTA POPULATION
+QUESTIONS
+
+1. use db.zipcodes.find() to filter results to only the results where city is ATLANTA
+and state is GA.
+2. use db.zipcodes.aggregate with $match to do the same as above.
+3. use $group to count the number of zip codes in Atlanta.
+4. use $group to find the total population in Atlanta.
+
+ANSWERS
+
+1)db.zipcodes.find({},{city:"ATLANTA",state:"GA"})
+2)db.zipcodes.find({$match:{city:"ATLANTA",state:"GA"}})
+3)db.zipcodes.aggregate({$group:{_id:"$city"}},{$count:"ATLANTA"})
+4)db.zipcodes.aggregate({$match:{city:"ATLANTA"}},{$group:{_id:"$pop",total:{$sum:"$pop"}}})
+
+#POPULATION BY STATE
+QUESTIONS
+
+1. use aggregate to calculate the total population for each state
+2. sort the results by population, highest first
+3. limit the results to just the first 3 results. What are the top 3 states in 
+population?
+
+ANSWERS
+
+1)db.zipcodes.aggregate({$group:{_id:{state:"$state",total:{$sum:"$pop"}}}})
+2)db.zipcodes.aggregate({$sort:{pop:-1}})
+3)db.zipcodes.aggregate({$sort:{pop:-1}},{$limit:3})
+  THE TOP THREE STATES ARE-IL,NY,NY
+
+#POPULATION BY CITY
+
+QUESTIONS
+
+1. use aggregate to calculate the total population for each city (you have to use
+city/state combination). You can use a combination for the _id of the $group: {
+city: '$city', state: '$state' }
+2. sort the results by population, highest first
+3. limit the results to just the first 3 results. What are the top 3 cities in
+population?
+4. What are the top 3 cities in population in Texas?
+
+ANSWERS
+
+1)db.zipcodes.aggregate({$group:{_id:{city:"$city",state:"$state",total:{$sum:"$pop"}}}})
+2)db.zipcodes.aggregate({$sort:{pop:-1}})
+3)db.zipcodes.aggregate({$sort:{pop:-1}},{$limit:3})
+4)Top three cities are: CHICAGO, BROOKLYN, NEW YORK
+
+#bonus
+
+QUESTIONS
+
+1. Write a query to get the average city population for each state.
+2. What are the top 3 states in terms of average city population
+
+ANSWERS
+
+1)db.zipcodes.aggregate({$group:{_id:{state:"$state",avg:{$avg:"$pop"}}}})
+2)db.zipcodes.aggregate({$group:{_id:{state:"state",avg:{$avg:"$pop"}}}},{$sort:{$pop:-1}})
---
+++ Assignment/mongo3.js
@@ -0,0 +1,114 @@
+#EXERCISE 
+
+QUESTIONS
+
+1. Write a MongoDB query to display all the documents in the collection 
+restaurants.
+2. Write a MongoDB query to display the fields restaurant_id, name, borough 
+and cuisine for all the documents in the collection restaurant.
+3. Write a MongoDB query to display the fields restaurant_id, name, borough 
+and cuisine, but exclude the field _id for all the documents in the collection 
+restaurant.
+4. Write a MongoDB query to display the fields restaurant_id, name, borough
+and zip code, but exclude the field _id for all the documents in the collection 
+restaurant.
+5. Write a MongoDB query to display the first 5 restaurant which is in the 
+borough Bronx.
+6. Write a MongoDB query to display all the restaurant which is in the borough 
+Bronx.
+7. Write a MongoDB query to display the next 5 restaurants after skipping first 5 
+which are in the borough Bronx.
+8. Write a MongoDB query to find the restaurants who achieved a score more 
+than 90.
+9. Write a MongoDB query to find the restaurants that achieved a score, more 
+than 80 but less than 100.
+10. Write a MongoDB query to find the restaurants which locate in latitude value 
+less than -95.754168.
+11. Write a MongoDB query to find the restaurants that do not prepare any 
+cuisine of 'American' and their grade score more than 70 and latitude less 
+than -65.754168.
+12. Write a MongoDB query to find the restaurants which do not prepare any 
+cuisine of 'American' and achieved a score more than 70 and located in the 
+longitude less than -65.754168.
+13. Write a MongoDB query to find the restaurants which do not prepare any 
+cuisine of 'American ' and achieved a grade point 'A' not belongs to the 
+borough Brooklyn. The document must be displayed according to the cuisine 
+in descending order.
+14. Write a MongoDB query to find the restaurant Id, name, borough and cuisine 
+for those restaurants which contain 'Wil' as first three letters for its name.
+15. Write a MongoDB query to find the restaurant Id, name, borough and cuisine 
+for those restaurants which contain 'ces' as last three letters for its name.
+16. Write a MongoDB query to find the restaurant Id, name, borough and cuisine 
+for those restaurants which contain 'Reg' as three letters somewhere in its 
+name.
+17. Write a MongoDB query to find the restaurants which belong to the borough 
+Bronx and prepared either American or Chinese dish.
+18. Write a MongoDB query to find the restaurant Id, name, borough and cuisine 
+for those restaurants which belong to the borough Staten Island or Queens or 
+Bronxor Brooklyn.
+19. Write a MongoDB query to find the restaurant Id, name, borough and cuisine 
+for those restaurants which are not belonging to the borough Staten Island or 
+Queens or Bronxor Brooklyn.
+20. Write a MongoDB query to find the restaurant Id, name, borough and cuisine 
+for those restaurants which achieved a score which is not more than 10.
+21. Write a MongoDB query to find the restaurant Id, name, borough and cuisine 
+for those restaurants which prepared dish except 'American' and 'Chinees' or 
+restaurant's name begins with letter 'Wil'.
+22. Write a MongoDB query to find the restaurant Id, name, and grades for those 
+restaurants which achieved a grade of "A" and scored 11 on an ISODate 
+"2014-08-11T00:00:00Z" among many of survey dates..
+23. Write a MongoDB query to find the restaurant Id, name and grades for those 
+restaurants where the 2nd element of grades array contains a grade of "A" 
+and score 9 on an ISODate "2014-08-11T00:00:00Z"
+24. Write a MongoDB query to find the restaurant Id, name, address and 
+geographical location for those restaurants where 2nd element of coord array 
+contains a value which is more than 42 and upto 52..
+25. Write a MongoDB query to arrange the name of the restaurants in ascending 
+order along with all the columns.
+26. Write a MongoDB query to arrange the name of the restaurants in descending 
+along with all the columns.
+27. Write a MongoDB query to arranged the name of the cuisine in ascending 
+order and for that same cuisine borough should be in descending order.
+28. Write a MongoDB query to know whether all the addresses contains the street 
+or not.
+29. Write a MongoDB query which will select all documents in the restaurants 
+collection where the coord field value is Double.
+30. Write a MongoDB query which will select the restaurant Id, name and grades 
+for those restaurants which returns 0 as a remainder after dividing the score 
+by 7.
+
+
+ANSWERS
+
+1)db.addresses.find()
+2)db.addresses.find({},{restaurant_id:1,name:1,borough:1,cuisine:1,})
+3)db.addresses.find({},{restaurant_id:1,name:1,borough:1,cuisine:1,_id:0})
+4)db.addresses.find({},{restaurant_id:1,name:1,borough:1,address:{zipcode:1},_id:0})
+
+5)db.addresses.find({borough:"bronx"}).limit(5)
+6)db.addresses.find({borough:"bronx"})
+7)db.addresses.find({borough:"bronx"}).skip(5)
+8)db.addresses.find({"grades.score":{$gt:90}})
+9)db.addresses.find({"grades.score":{$gt:80,$lt:100}})
+10)db.addresses.find({"address.coord":{$lt:-95.754168}})
+11)db.addresses.finf({$and:[{cuisine:{$ne:"American "}},{"grades.score":{$gt:70}},{"address.coord":{$lt:-65.754168}}]})
+12)db.addresses.find({$and:[{cuisine:{$ne:"American "}},{"grades.score":{$gt:70}},{"address.coord":{$lt:-65.754168}}]})
+	
+13)db.addresses.find({$and:[{cuisine:{$ne:"American "}},{"grades.grade":"A"},{borough:{$ne:"brooklyn"}}]}).sort({cuisine:-1})
+14)db.addresses.find({name:/^Wil/},{restaurant_id:1,name:1,borough:1,cuisine:1})
+15)db.addresses.find({name:/ces$/},{restaurant_id:1,name:1,borough:1,cuisine:1})
+16)db.addresses.find({name:/.*Reg.*/},{restaurant_id:1,name:1,borough:1,cuisine:1})
+17)db.addresses.find({borough:"Bronx",$or:[{"cuisine:"American"},{"cuisine":"Chinese"}]})
+18)db.addresses.find({borough:{$in:["Staten Island","Queens","Bronxor Brooklyn"]}},{restaurant_id:1,name:1,borough:1,cuisine:1})
+19)db.addresses.find({borough:{$nin:["Staten Island","Queens","Bronxor Brooklyn"]}},{restaurant_id:1,name:1,borough:1,cuisine:1})
+20)db.addresses.find({"grades.score":{$lt:10}},{restaurant_id:1,name:1,borough:1,cuisine:1})
+21)db.addresses.find({cuisine:{$nin:["American ","Chinese"]}},{restaurant_id:1,name:1,borough:1,cuisine:1})
+22)db.addresses.find({"grades.grade":"A","grades.date":ISODate("2014-08-11T00:00:00Z"),"grades.score":11},{restaurant_id:1,name:1,grades:1})
+23)db.addresses.find({"grades.1.grade":"A","grades.1.score":9,"grades.1.date":ISODate("2014-08-1100:00:00Z")},{restaurant_id:1,name:1,grades:1})
+24)db.addresses.find({"address.coord.1":{$gt:42,$lte:52}},{restaurant_id:1,name:1,address:1,coord:1})
+25)db.addresses.find().sort({name:1})
+26)db.addresses.find().sort({name:-1})
+27)db.addresses.find().sort({cuisine:1,borough:-1})
+28)db.addresses.find({"address.street":{$exists:true}})
+29)db.addresses.find({"address.cord":{$type:1}})
+30)db.addresses.find({"grades.score":{$mod:[7,0]}})
