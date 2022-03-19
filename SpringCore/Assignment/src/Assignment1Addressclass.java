8589bf7678d988cb813fb5a3c08356fa99ac164b
Vishnu Pillai <65006856+vishnupillai704@users.noreply.github.com>
2022-03-19 11:34:34 +0530

    Assignment1 files

---
+++ SpringCore/Assignment/src/Assignment1Addressclass.java
@@ -0,0 +1,50 @@
+package com.example.demo;
+
+public class Assignment1Addressclass {
+    private String street;
+    private String city;
+    private String state;
+    private Integer zip ;
+    private String country;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Integer getZip() {
+        return zip;
+    }
+
+    public void setZip(Integer zip) {
+        this.zip = zip;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+}
---
+++ SpringCore/Assignment/src/Assignment1Customerclass.java
@@ -0,0 +1,65 @@
+package com.example.demo;
+
+public class Assignment1Customerclass {
+    private Assignment1Addressclass addressclass;
+    private Integer customerId;
+    private String customerName;
+    private Integer customerContact;
+    private String customerAddress;
+
+    public Assignment1Customerclass(Assignment1Addressclass addressclass, Integer customerId, String customerName, Integer customerContact, String customerAddress) {
+        this.addressclass = addressclass;
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.customerContact = customerContact;
+        this.customerAddress = customerAddress;
+    }
+    public void Details(){
+        System.out.println(" customer id: "+this.customerId+"customer name: "+this.customerName+" customerContact: "+this.customerContact+" customerAddress"+this.customerAddress+ " "+addressclass.getCountry()+" "+addressclass.getState()+" "+addressclass.getCity()+" "+addressclass.getStreet()+" "+addressclass.getZip());
+    }
+
+//  public Integer getCustomerId() {
+//       return customerId;
+//    }
+//
+//    public void setCustomerId(Integer customerId) {
+//        this.customerId = customerId;
+//    }
+//
+//    public String getCustomerName() {
+//        return customerName;
+//    }
+//
+//    public void setCustomerName(String customerName) {
+//        this.customerName = customerName;
+//    }
+//
+//    public Integer getCustomerContact() {
+//        return customerContact;
+//    }
+//
+//    public void setCustomerContact(Integer customerContact) {
+//        this.customerContact = customerContact;
+//    }
+//
+//    public String getCustomerAddress() {
+//        return customerAddress;
+//    }
+//
+//    public void setCustomerAddress(String customerAddress) {
+//        this.customerAddress = customerAddress;
+//    }
+//
+//
+//
+//    public Assignment1Addressclass getAddressclass() {
+//        return addressclass;
+//    }
+//
+//    public void setAddressclass(Assignment1Addressclass addressclass) {
+//        this.addressclass = addressclass;
+//    }
+//    public void Details(){
+//        System.out.println("Customer id :"+getCustomerId()+" CustomerName : "+getCustomerName()+" CustomerContact: "+customerContact+" CustomerAddress : "+getCustomerAddress()+" "+addressclass.getCountry()+" "+addressclass.getState()+" "+addressclass.getCity()+" "+addressclass.getStreet()+" "+addressclass.getZip());
+//    }
+}
